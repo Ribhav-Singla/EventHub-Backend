136c99ebc4e3013f927b4654c7e8f67ca0a12854
ribhavsingla <ribhavsingla2166@gmail.com>
2025-03-11 16:00:23 +0530

    fixed typo in vercel.json

--- dist/http/index.js
+++ dist/http/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -17,6 +26,9 @@ const PORT = process.env.PORT || 3000;
 app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 app.use(express_1.default.static('uploads/'));
+app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.send('Server is running');
+}));
 app.use('/api/v1', v1_1.router);
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
